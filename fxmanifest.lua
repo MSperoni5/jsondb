c6c2f4b16862f2c3c8327f7ad583e769d6653e9c
MSperoni5 <m.speroni5@campus.unimib.it>
2024-04-29 22:53:59 +0200

    UPDATE 29-04-2024 22:53

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,14 @@
 fx_version 'cerulean'
-games { 
+games {
     'rdr3',
-    'gta5' 
+    'gta5'
 }
 lua54 'yes'
 author 'MSperoni5 <https://github.com/MSperoni5>'
 description 'Lua script to manage a database using JSON files as a data store.'
 version '1.0.0'
 server_only 'yes'
-server_scripts { 
+server_scripts {
     'server/ErrorsHandler.lua',
     'server/FileManager.lua',
     'server/DatabaseHandler.lua',
